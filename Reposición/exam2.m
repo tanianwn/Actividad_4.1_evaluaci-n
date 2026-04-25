57b10f9ff0a96e260adf4591fe953ab89fb958e1
Tania <125945310+tanianwn@users.noreply.github.com>
2026-04-24 18:13:58 -0600

    Add simulation for robot movement and visualization

--- Reposición/exam2.m
+++ Reposición/exam2.m
@@ -1,1 +1,126 @@
 
+clear
+close all
+clc
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TIEMPO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+tf = 10;             % Tiempo de simulacion (ajustado al dominio)
+ts = 0.05;            
+t = 0: ts: tf;       
+N = length(t);       
+
+%%%%%%%%%%%%%%%%%%%%%%%% CONDICIONES INICIALES %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+x1 = zeros(1,N+1);  
+y1 = zeros(1,N+1);  
+phi = zeros(1,N+1); 
+
+% Condiciones de la trayectoria específicas
+x1(1) = 0;              
+y1(1) = 0;              
+phi(1) = atan(2);       
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% PUNTO DE CONTROL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+hx = zeros(1,N+1);  
+hy = zeros(1,N+1);  
+
+hx(1) = x1(1);
+hy(1) = y1(1);
+
+%%%%%%%%%%%%%%%%%%%%%% VELOCIDADES DE REFERENCIA %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+u = zeros(1,N); % velocidad lineal
+w = zeros(1,N); % velocidad angular
+
+for k = 1:N
+    %x=t
+    %y=2*sen(t^2)
+    %t= [0 a 5]
+    %θ(𝒕) = t𝒂𝒏^−𝟏(4*cos(t^2)−8t^2*sin(t^2))
+    tk = t(k);
+    
+    dx = 1;
+    dy = 2 * exp(0.3 * tk) * (0.3 * sin(tk)+cos(tk));
+    
+    % Velocidad lineal
+    u(k) = sqrt(dx^2 + dy^2);
+    
+    % Velocidad angular
+    w(k) = (exp(0.3 * tk) * (1.2 * cos(tk) - 1.82 * sin(tk))) / (1 + (2 * exp(0.3 * tk) * (cos(tk) + 0.3 * sin(tk)))^2);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%% BUCLE DE SIMULACION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for k=1:N 
+    
+    phi(k+1)=phi(k)+w(k)*ts; % Integral numérica (método de Euler)
+    
+    %%%%%%%%%%%%%%%%%%%%% MODELO CINEMATICO %%%%%%%%%%%%%%%%%%%%%%%%%
+    %Aplicamos el modelo cinemático diferencial para obtener las
+    %velocidades en x, y, phi
+    xp1=u(k)*cos(phi(k+1)); 
+    yp1=u(k)*sin(phi(k+1));
+    phip= w(k)
+
+    x1(k+1)=x1(k) + xp1*ts ; % Integral numérica (método de Euler)
+    y1(k+1)=y1(k) + yp1*ts ; % Integral numérica (método de Euler)
+    
+    
+    % Posicion del robot con respecto al punto de control
+    hx(k+1)=x1(k+1); 
+    hy(k+1)=y1(k+1);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SIMULACION VIRTUAL 3D %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% a) Configuracion de escena
+
+scene=figure;  % Crear figura (Escena)
+set(scene,'Color','white'); % Color del fondo de la escena
+set(gca,'FontWeight','bold') ;% Negrilla en los ejes y etiquetas
+sizeScreen=get(0,'ScreenSize'); % Retorna el tamaño de la pantalla del computador
+set(scene,'position',sizeScreen); % Congigurar tamaño de la figura
+camlight('headlight'); % Luz para la escena
+axis equal; % Establece la relación de aspecto para que las unidades de datos sean las mismas en todas las direcciones.
+grid on; % Mostrar líneas de cuadrícula en los ejes
+box on; % Mostrar contorno de ejes
+xlabel('x(m)'); ylabel('y(m)'); zlabel('z(m)'); % Etiqueta de los eje
+
+view([25 25]); % Orientacion de la figura
+axis([-3 20 -15 24 0 2]); % Ingresar limites minimos y maximos en los ejes x y z [minX maxX minY maxY minZ maxZ]
+
+% b) Graficar robots en la posicion inicial
+scale = 4;
+MobileRobot_5;
+H1=MobilePlot_4(x1(1),y1(1),phi(1),scale);hold on;
+
+% c) Graficar Trayectorias
+H2=plot3(hx(1),hy(1),0,'r','lineWidth',2);
+
+% d) Bucle de simulacion de movimiento del robot
+
+step=1; % pasos para simulacion
+
+for k=1:step:N
+
+    delete(H1);    
+    delete(H2);
+    
+    H1=MobilePlot_4(x1(k),y1(k),phi(k),scale);
+    H2=plot3(hx(1:k),hy(1:k),zeros(1,k),'r','lineWidth',2);
+    
+    pause(ts);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Graficas %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+graph=figure;  % Crear figura (Escena)
+set(graph,'position',sizeScreen); % Congigurar tamaño de la figura
+subplot(211)
+plot(t,u,'b','LineWidth',2),grid('on'),xlabel('Tiempo [s]'),ylabel('m/s'),legend('u');
+subplot(212)
+plot(t,w,'r','LineWidth',2),grid('on'),xlabel('Tiempo [s]'),ylabel('[rad/s]'),legend('w');
+
